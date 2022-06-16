cb648fd663d73e55170aa945532aab2ab54c06e0
jenn39539 <jennycarol2001@gmail.com>
2022-06-16 10:15:52 -0700

    5.1

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include <stidio.h>
+#include <stdio.h>
 
 /**
  * main - Firts
- * 
+ *
  * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("With proper grammar, but the outcome is a piece of art, \n");
 	return (0)
 }
