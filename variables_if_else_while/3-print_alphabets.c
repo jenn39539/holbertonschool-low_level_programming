a8320b68fcc8cd641e13c4ffbb35f54973f70eb6
jenn39539 <jennycarol2001@gmail.com>
2022-06-18 16:26:12 -0700

    aZ

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main - alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char al;
+	char alm;
+
+	for (al = 'a' ; al <= 'z' ; al++)
+	{
+		putchar(al);
+	}
+	for (alm 'A' ; alm <= 'Z' alm ; alm++)
+	{
+		putchar(alm);
+	}
+	putchar(alm);
+}
+putchar('\n');
+return (0);
+}
