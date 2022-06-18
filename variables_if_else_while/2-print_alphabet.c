fa27c909d008a9305a96ce4f0a0a06b9a8c93674
jenn39539 <jennycarol2001@gmail.com>
2022-06-18 16:16:59 -0700

    put

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+	char al;
+
+	for (al = 'a' ; al <= 'z' ; al++)
+	{
+		putchar(al);
+	}
+	putchar('\n');
+	return (0);
+}	
