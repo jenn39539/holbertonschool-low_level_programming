96a6f4177b999f1dcc27431ab9a36adc25adb124
jenn39539 <jennycarol2001@gmail.com>
2022-06-18 16:44:30 -0700

    est

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - numbers
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0' ; n <= '9' ; n++)
+	{
+		putchat(n);
+	}
+	putchar('\n');
+	return (0);
+}
