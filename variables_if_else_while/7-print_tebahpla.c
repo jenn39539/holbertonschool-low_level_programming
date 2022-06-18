5767924254a7cc3f4926999f828683a06ef1ceea
jenn39539 <jennycarol2001@gmail.com>
2022-06-18 16:58:45 -0700

    je

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - tebahpla.c
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	char la;
+
+	for (la = 'z' ; la >= 'a' ; la--)
+	{
+		putchar(la);
+	}
+	putchar('\n');
+	return (0);
+}
