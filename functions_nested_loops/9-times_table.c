44181f9cc73d386e08ef358defb54ed425a093c0
jenn39539 <jennycarol2001@gmail.com>
2022-06-22 13:35:09 -0700

    k

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -11,16 +11,17 @@ void times_table(void)
 		for (n = 0 ; n <= 9 ; n++)
 	{
 		_putchar('0');
-		for (t = 1 ; t <= 9 ; n++)
-
-		_putchar(' , ');
-		_putchar('  ');
-		m = (n * t);
-		if (m < 10)
+		for (t = 1 ; t <= 9 ; t++)
+		{
+			_putchar(',');
 			_putchar(' ');
-		else
-			_putchar((m / 10) + 48);
-		_putchar((m % 10) + 48);
+			m = (n * t);
+			if (m < 10)
+				_putchar(' ');
+			else
+				_putchar((m / 10) + 48);
+			_putchar((m % 10) + 48);
 	}
-	_putchar('\n');
+		_putchar('\n');
+}
 }
