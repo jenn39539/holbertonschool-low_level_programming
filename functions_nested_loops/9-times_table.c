57362653fefc1a6ea376a5c3079d33e5e2db058c
jenn39539 <jennycarol2001@gmail.com>
2022-06-22 13:02:29 -0700

    2apr

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * times_table - mult
+ *
+ *
+ */
+void times_table(void)
+{
+	int n, t, m;
+		for (n = 0 ; n <= 9 ; t++)
+	{
+		_putchar(',');
+		_putchar(' ');
+		m = (n * t);
+		if (m < 10)
+			_putchar(' ');
+		else
+			putchar((m / 10) + 48);
+		_putchar((m % 10) + 48);
+	}
+	_putchar('\n');
+}
+}
